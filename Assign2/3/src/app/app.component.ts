d76ed8fe5bb3caf6f7647a9261b2bd1564440036
armaanshaikh <38371075+armaanshaikh@users.noreply.github.com>
2018-06-24 14:31:14 +0530

    Assignment Second

---
+++ Assign2/3/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<input type="text" class="txt" name="item">
+  Marvellous Infosystem`,
+  styles: [`input{
+    color:blue
+    }`],
+
+})
+export class AppComponent {
+ 
+}
